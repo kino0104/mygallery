6c05ec3f6c02dfce617f96a7b7c575b57a5b7e4c
きのした <kinop0506@gmail.com>
2014-09-15 21:09:21 +0900

    template.php

---
+++ contents/template.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+		<meta charset="utf-8">
+        <title>Kino Photography</title>
+        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript"  src="./js/lightbox.js"></script>
+       　<script src="js/myscript.js"></script>
+        <!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<![endif]-->
+
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.6.0/build/cssreset/cssreset-min.css" />
+		<link rel="stylesheet" media="screen" href="css/mycss.css" />
+		<link rel="stylesheet" type="text/css" href="css/lightbox.css" />
+	</head>
+    <body>
+    <div id="container">
+    <div id="header">
+    	<div id="title">
+    		<h1>Kyousuke Kinoshita</h1>
+    		
+    	</div>
+    	<div id="menu">
+    		<ul>
+    		<li><a href="index.html">Top</a></li>
+    		<li><a href="about.html">About</a></li>
+    		<li><a href="kino_0104.html">Gallery</a></li>
+    		<li><a href="contact.html">Contact</a></li>
+    		</ul>
+    	</div>
+    </div>
+
+    <div id="main">
+	<div id="mygallery"></div>
+	</div>
+
+    <div id="footer">© 2014 Kyousuke Kinoshita</div>
+
+
+    </div><!-- container -->
+    </body>
+</html>
