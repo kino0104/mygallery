fe6b325808401ab9de6e5fa42167ac4a3f8d9171
きのした <kinop0506@gmail.com>
2014-09-23 12:21:20 +0900

    Update gallery.php

--- contents/gallery.php
+++ contents/gallery.php
@@ -5,6 +5,7 @@ include('connection.php');
 
 session_start();
 
+$username = $_SESSION['user']['username'];
 if(empty($_SESSION['user'])){
 	header('Location: '.SITE_URL.'login.php');
 	exit;
@@ -16,7 +17,8 @@ if(empty($_SESSION['user'])){
 <html lang="ja">
     <head>
 		<meta charset="utf-8">
-        <title>Kino Photography</title>
+		<meta name="viewport" content="width=device-width">
+        <title>InstaSS</title>
         <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 
         <script src="js/myscript.js"></script>
@@ -30,21 +32,18 @@ if(empty($_SESSION['user'])){
     <div id="container">
     <div id="header">
     	<div id="title">
-    		<h1>Kyousuke Kinoshita</h1>
-    		
+    		<h1>InstaSS</h1> 		
     	</div>
     	<div id="menu">
-    		<ul>
-    		<li><a href="index.html">Top</a></li>
-    		<li><a href="about.html">About</a></li>
-    		<li><a href="gallery.php">Gallery</a></li>
-    		<li><a href="contact.html">Contact</a></li>
-    		</ul>
+			<p>Hello, <?php echo h($username); ?>!<br/>
+				お気に入り写真をクリックして<br/>
+				Instagramにアップした写真からSolo Show（個展）を作成してください！</p>
     	</div>
     </div>
 
     <div id="main">
     <!-- 写真並び替え処理へ -->
+    <div id="form">
     <form class="gform" action="sort.php" method="post">
 		<input type="submit" id="sort" name="photos" value="並び替え" />
 	</form>
@@ -56,17 +55,17 @@ if(empty($_SESSION['user'])){
 	
 	<!-- 写真を再取得 -->
 	<form class="gform" action="reset.php" method="get">
-		<input type="submit" value="リセット" />
+		<input type="submit" id="reset" value="リセット" />
 	</form>
-	
+	</div>
 	<div id="logout">
-	<a href="logout.php">[ログアウト]</a>
+	<a href="logout.php">ログアウト</a>
 	</div>
 	<div id="makegallery"></div>
 
 	</div>
 
-    <div id="footer">Copyright 2014 Kyousuke Kinoshita</div>
+    <div id="footer">Copyright 2014 instass</div>
 
 
     </div><!-- container -->
