975fd73bfceba60d261d052755ed2b46652fdbb1
きのした <kinop0506@gmail.com>
2014-09-23 12:24:43 +0900

    Update register.php

--- contents/register.php
+++ contents/register.php
@@ -3,12 +3,13 @@ require_once('config.php');
 include('connection.php');
 
 session_start();
-
+$username = $_SESSION['user']['username'];
 // instagramユーザ名でファイル名を生成
-$template = "http://localhost/api/template.php";
+$template = "http://instass.xyz/template.php";
 
 $filename = $_SESSION['user']['username'].'.html';
 $contents = file_get_contents($template);
-
+$contents = str_replace("TITLE", $username."'s Gallery", $contents);
+$contents = str_replace("HEADER1", $username."'s Gallery", $contents);
 file_put_contents($filename, $contents);
 header('Location: '.$filename);
