a67cac1a95c37305011872c87a652f4311782c6c
きのした <kinop0506@gmail.com>
2014-09-23 12:25:39 +0900

    Update template.php

--- contents/template.php
+++ contents/template.php
@@ -2,7 +2,8 @@
 <html lang="ja">
     <head>
 		<meta charset="utf-8">
-        <title>Kino Photography</title>
+		<meta name="viewport" content="width=device-width">
+        <title>TITLE</title>
         <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
         <script type="text/javascript"  src="./js/lightbox.js"></script>
        　<script src="js/myscript.js"></script>
@@ -18,24 +19,17 @@
     <div id="container">
     <div id="header">
     	<div id="title">
-    		<h1>Kyousuke Kinoshita</h1>
+    		<h1>HEADER1</h1>
     		
     	</div>
-    	<div id="menu">
-    		<ul>
-    		<li><a href="index.html">Top</a></li>
-    		<li><a href="about.html">About</a></li>
-    		<li><a href="kino_0104.html">Gallery</a></li>
-    		<li><a href="contact.html">Contact</a></li>
-    		</ul>
-    	</div>
+
     </div>
 
     <div id="main">
 	<div id="mygallery"></div>
 	</div>
 
-    <div id="footer">© 2014 Kyousuke Kinoshita</div>
+    <div id="footer">© 2014 <a href="http://localhost/api/login.php">InstaSS</a> by <a href="http://gallery.kinokk.com">Kyousuke Kinoshita</a></div>
 
 
     </div><!-- container -->
